8c627674dac4c0e96ea689db6c88236b495bd66a
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-06-04 13:16:27 +0900

    桁数をチェックしてから処理を分けるように変更した
    if文からcase文への変更
    メソッドの使用方法を学んだ

--- english_numerals.rb
+++ english_numerals.rb
@@ -1,12 +1,26 @@
-num =ARGV.first.to_i 
+num = ARGV.first.to_i
 
-if num < 10 then
-  count = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-  puts count[num]
-elsif num >= 10 && num < 20 then
-  num = num - 10
-  count = ["ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
-  puts count[num]
-elsif num >= 20 then
-  puts "overTwenty"
+def english_numeral(num)
+  len = ARGV.first.length
+  case len
+  when 1
+    count = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    numeral = count[num]
+  when 2
+    if num >= 10 && num < 20 then
+      num = num - 10
+      count = ["ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+      numeral = count[num]
+    elsif num >= 20 then
+      numeral = "overTwenty"
+    end
+  end
+  return numeral
 end
+
+if num == 0
+ numeral = "zero"
+else
+ numeral = english_numeral(num)
+end
+puts numeral
