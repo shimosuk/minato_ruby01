f4965183aa896c91dce5b55b0071393bcb20046b
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-06-06 12:41:02 +0900

    4ケタの英語表示に一部対応した
    ただし、値の中に0が入ることを考慮していない

--- english_numerals.rb
+++ english_numerals.rb
@@ -1,5 +1,11 @@
 num = ARGV.first.to_i
 
+def quadruple(num)
+  qnum = num.to_s[0]
+  tnum = num.to_s[1..3]
+  "#{single(qnum.to_i)} thousand #{triple(tnum.to_i)}"
+end
+
 def triple(num)
   tnum = num.to_s[0]
   num = num.to_s[1..2]
@@ -34,6 +40,8 @@ def english_numeral(num)
   when 3
     triple(num)
   when 4
+    quadruple(num)
+  when 5
     "over"
   end
 end
