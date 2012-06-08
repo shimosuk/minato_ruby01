5c4258bce4cf6f538964e1fe46e3a1f5692fc713
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-06-08 09:30:12 +0900

    doubleがRSpecの予約後であったため、メソッド名を変更並びに他のメソッドのテスト追加
    メソッドに'_digit'をつけた

--- english_numerals.rb
+++ english_numerals.rb
@@ -1,18 +1,25 @@
 num = ARGV.first
 
-def quadruple(num)
+def exclude_zero(num)
+  #TODO 01 00 の0を除外する処理を書く。ただし、1ケタは必ず残す
+  check_digit(num)
+end
+
+#TODO 1011や1001,1000に未対応
+def quadruple_digit(num)
   qnum = num[0]
   tnum = num[1..3]
-  "#{single(qnum)} thousand #{triple(tnum)}"
+  "#{single_digit(qnum)} thousand #{triple_digit(tnum)}"# triple_digitをexclude_zeroに
 end
 
-def triple(num)
+#TODO 100や101に未対応
+def triple_digit(num)
   tnum = num[0]
   num = num[1..2]
-  "#{single(tnum.to_i)} hundread #{double(num)}"
+  "#{single_digit(tnum.to_i)} hundread #{double_digit(num)}"# 上記同様
 end
 
-def double(num)
+def double_digit(num)
   if num.to_i < 20
     n = num.to_i - 10
     count = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
@@ -21,11 +28,11 @@ def double(num)
     snum = num[1]
     dnum = num[0].to_i
     count = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-    "#{count[dnum - 2]} #{single(snum)}"
+    "#{count[dnum - 2]} #{single_digit(snum)}"
   end
 end
 
-def single(num)
+def single_digit(num)
   count = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
   count[num.to_i]
 end
@@ -34,13 +41,13 @@ def check_digit(num)
   digit = num.length
   case digit
   when 1
-    single(num)
+    single_digit(num)
   when 2
-    double(num)
+    double_digit(num)
   when 3
-    triple(num)
+    triple_digit(num)
   when 4
-    quadruple(num)
+    quadruple_digit(num)
   when 5
     "over"
   end
--- english_numerals_spec.rb
+++ english_numerals_spec.rb
@@ -4,35 +4,27 @@ require 'english_numerals'
 describe "test_single_digit" do 
 
   specify do
-    single("").should == ""
-    single("5").should == "five"
-    single("9").should == "nine"
+    single_digit("").should == ""
+    single_digit("5").should == "five"
+    single_digit("9").should == "nine"
   end
   specify do
-    single("10").should == nil
-    single("111").should == nil
+    single_digit("10").should == nil
+    single_digit("111").should == nil
   end
 end
 
 describe "test_double_digit" do 
-=begin
-  specify{double("11").should == "eleven"}
-  specify{double("19").should == "nineteen"}
-  specify{double("28").should == "twnty eight"}
-=end
-end
-
-if check_digit("12") == double("12")
-  p true
+  specify{double_digit("11").should == "eleven"}
+  specify{double_digit("19").should == "nineteen"}
+  specify{double_digit("28").should == "twenty eight"}
 end
 
 describe "test_check_digit" do 
-  specify{check_digit("1").should == single("1")}
-=begin
-  specify{check_digit("12").should == double("12")}
-  specify{check_digit("23").should == double("23")}
-  specify{check_digit("123").should == triple("123")}
-  specify{check_digit("1234").should == quadruple("123")}
-=end
+  specify{check_digit("1").should == single_digit("1")}
+  specify{check_digit("12").should == double_digit("12")}
+  specify{check_digit("23").should == double_digit("23")}
+  specify{check_digit("123").should == triple_digit("123")}
+  specify{check_digit("1234").should == quadruple_digit("1234")}
 end
 
