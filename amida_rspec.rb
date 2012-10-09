e3b77fe8da29e89afb900a083e5849764a8bec76
ryosuke shimomura <shimosuk.shimosuke@gmail.com>
2012-10-09 09:56:52 +0900

    テストを追加

--- amida_rspec.rb
+++ amida_rspec.rb
@@ -2,8 +2,16 @@ $LOAD_PATH << File.dirname(__FILE__)
 require 'amida'
 
 
-describe "go_straight" do
+describe "if_user_is_one,go_straight_only" do
+  before do
+    @hight = 4
+  end
+  subject{ stalk(1) }
+  it { should == "|" }
+end
+
+describe "junction" do
   specify do
-     stalk(0).should == "|"
+    stalk(2).should == "|"
   end
 end
